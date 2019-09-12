0c2a4397c0a18204ebc03eb5538673657e887829
Paul Bauer <paul@thebauers.net>
2019-09-11 19:23:02 -0600

    almost though page 266

--- settings.py
+++ settings.py
@@ -15,3 +15,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
+
+        # Alien settings
+        self.alien.speed = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        
